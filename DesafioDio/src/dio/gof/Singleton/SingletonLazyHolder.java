4af8380b75bf78921f9ee4f3586d38de87169cb6
Levysq <levyqggt@gmail.com>
2022-06-24 13:50:57 -0300

    first commit

---
+++ DesafioDio/src/dio/gof/Singleton/SingletonLazyHolder.java
@@ -0,0 +1,15 @@
+package dio.gof.Singleton;
+
+public class SingletonLazyHolder {
+	private static class IntanceHolder {
+		public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+	}
+	
+	private SingletonLazyHolder() {
+		super();
+	}
+	
+	public static SingletonLazyHolder getInstancia() {
+		return IntanceHolder.instancia;
+	}
+}
